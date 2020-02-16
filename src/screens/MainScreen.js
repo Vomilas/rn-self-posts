2d573770429ef08085669fd60bb6ad625aac10ec
Anush Salimov <safmail96@gmail.com>
2020-02-16 21:16:05 +0300

    add header icons

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { DATA } from "../data";
 import { Post } from "../components/Post";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = post => {
@@ -19,7 +21,16 @@ export const MainScreen = ({ navigation }) => {
 };
 
 MainScreen.navigationOptions = {
-  headerTitle: "My blog"
+  headerTitle: "My blog",
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Take photo"
+        iconName="ios-camera"
+        onPress={() => console.log("Press photo")}
+      />
+    </HeaderButtons>
+  )
 };
 
 const styles = StyleSheet.create({
