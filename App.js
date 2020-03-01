eaca4bc507e57ecf62a461b47a949335bcc7e362
Anush Salimov <safmail96@gmail.com>
2020-03-01 14:25:48 +0300

    add redux actions and reducers

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { AppLoading } from "expo";
+import { Provider } from "react-redux";
 import { AppNavigation } from "./src/navigation/AppNavigation";
 import { bootstrap } from "./src/bootstrap";
+import store from "./src/store";
 
 export default function App() {
-  const [isReady, setReady] = useState(false);
+  const [isReady, setIsReady] = useState(false);
 
   if (!isReady) {
     return (
       <AppLoading
         startAsync={bootstrap}
-        onFinish={() => setReady(true)}
+        onFinish={() => setIsReady(true)}
         onError={err => console.log(err)}
       />
     );
   }
 
-  return <AppNavigation />;
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  );
 }
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import {
   View,
   Text,
@@ -8,15 +8,37 @@ import {
   ScrollView,
   Alert
 } from "react-native";
-import { DATA } from "../data";
+import { useDispatch, useSelector } from "react-redux";
 import { THEME } from "../theme";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { removePost, toggleBooked } from "../store/actions/post";
 
 export const PostScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+
   const postId = navigation.getParam("postId");
 
-  const post = DATA.find(p => p.id === postId);
+  const post = useSelector(state =>
+    state.post.allPosts.find(p => p.id === postId)
+  );
+
+  const booked = useSelector(state =>
+    state.post.bookedPosts.some(post => post.id === postId)
+  );
+
+  useEffect(() => {
+    navigation.setParams({ booked });
+  }, [booked]);
+
+  const toggleHandler = useCallback(() => dispatch(toggleBooked(postId)), [
+    dispatch,
+    postId
+  ]);
+
+  useEffect(() => {
+    navigation.setParams({ toggleHandler });
+  }, [toggleHandler]);
 
   const removeHandler = () => {
     Alert.alert(
@@ -27,11 +49,23 @@ export const PostScreen = ({ navigation }) => {
           text: "Cancel",
           style: "cancel"
         },
-        { text: "Remove", style: "destructive", onPress: () => {} }
+        {
+          text: "Remove",
+          style: "destructive",
+          onPress() {
+            navigation.navigate("Main");
+            dispatch(removePost(postId));
+          }
+        }
       ],
       { cancelable: false }
     );
   };
+
+  if (!post) {
+    return null;
+  }
+
   return (
     <ScrollView>
       <Image source={{ uri: post.img }} style={styles.image} />
@@ -50,6 +84,7 @@ export const PostScreen = ({ navigation }) => {
 PostScreen.navigationOptions = ({ navigation }) => {
   const date = navigation.getParam("date");
   const booked = navigation.getParam("booked");
+  const toggleHandler = navigation.getParam("toggleHandler");
   const iconName = booked ? "ios-star" : "ios-star-outline";
 
   return {
@@ -59,7 +94,7 @@ PostScreen.navigationOptions = ({ navigation }) => {
         <Item
           title="Toggle drawer"
           iconName={iconName}
-          onPress={() => console.log("Press photo")}
+          onPress={toggleHandler}
         />
       </HeaderButtons>
     )
